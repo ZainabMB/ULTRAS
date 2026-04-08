3b11abdc8521ff29f38e5a7f9bbbe87e29085234
ZainabMB <zainabmbdd@gmail.com>
2026-04-08 22:22:19 +0100

    all db syncs completed

---
+++ app/src/main/java/com/example/repository/LeagueRepository.java
@@ -0,0 +1,7 @@
+package com.example.repository;
+
+import com.example.model.League;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LeagueRepository extends JpaRepository<League, Long> {
+}
